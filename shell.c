5276b44afb71db73a47be7cd74a69f92d701ac54
Lumsta01 <lulamamanganyi01@gmail.com>
2023-02-21 18:58:47 -0800

    shell update

--- shell.c
+++ shell.c
@@ -1,19 +1,81 @@
 #include "shell.h"
 
 /**
- *main - shell prompt
- *Return: 0
+ * main - execution
+ * Return: 0
  */
+
 int main(void)
 {
-	char *buffer = malloc(1024);
-	size_t len = 1024;
 
-	while (1)
+	char *ln, **command;
+	int st = 1;
+
+	signal(SIGINT, handler_function);
+	do
+
 	{
-		printf("$ ");
-		getline(&buffer, &len, stdin);
-		printf("%s", buffer);
-	}
+		if (isatty(STDIN_FILENO))
+			printf("$ ");
+
+		else
+		{
+			ln = getln();
+			command = formatln(ln);
+			search_exe(command);
+			launch_process(command);
+			free(ln);
+			free(command);
+			return (0);
+		}
+
+		ln = getln();
+		command = formatln(ln);
+		if (*command == NULL)
+		{
+			free(ln);
+			free(command);
+			continue;
+		}
+		search_exe(command);
+		st = launch_process(command);
+		free(ln);
+		free(command);
+
+	} while (st);
+
 	return (0);
+}
+
+/**
+ * launch_process - execute command
+ * @command: command
+ * Return: 1
+ */
+
+int launch_process(char **command)
+{
+	pid_t pid;
+
+	pid = fork();
+	if (pid == 0 && execve(command[0], command, environ) == -1)
+		perror("./shell");
+
+	else
+		wait(&pid);
+
+	return (1);
+}
+
+/**
+ * handler_function - handle signit
+ * @i: integer
+ */
+
+void handler_function(int i)
+{
+	if (i)
+	{
+		write(STDOUT_FILENO, "\n$ ", strlen("\n$ "));
+	}
 }
