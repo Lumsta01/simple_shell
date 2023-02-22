eb869d6c9864592d8e59a215d1fec80ca12a9dc8
Lumsta01 <lulamamanganyi01@gmail.com>
2023-02-21 18:39:25 -0800

    shell prompt update

---
+++ shell.c
@@ -0,0 +1,19 @@
+#include "shell.h"
+
+/**
+ *main - shell prompt
+ *Return: 0
+ */
+int main(void)
+{
+	char *buffer = malloc(1024);
+	size_t len = 1024;
+
+	while (1)
+	{
+		printf("$ ");
+		getline(&buffer, &len, stdin);
+		printf("%s", buffer);
+	}
+	return (0);
+}
